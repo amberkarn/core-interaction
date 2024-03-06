400c972882f69ac93365bc9d62d471e12b875afb
amberkarn <pjonk811@newschool.edu>
2024-03-06 15:19:39 -0500

    commit

--- project1/main.js
+++ project1/main.js
@@ -9,46 +9,34 @@ function turnOnLight() {
 let button = document.querySelector(".button");
 button.addEventListener("click", turnOnLight);
 
-// Make the DIV element draggable:
-dragElement(document.getElementById("mydiv"));
+const numberThree = document.querySelector('.numberThree');
 
-function dragElement(elmnt) {
-  var pos1 = 0, pos2 = 0, pos3 = 0, pos4 = 0;
-  if (document.getElementById(elmnt.id + "header")) {
-    // if present, the header is where you move the DIV from:
-    document.getElementById(elmnt.id + "header").onmousedown = dragMouseDown;
-  } else {
-    // otherwise, move the DIV from anywhere inside the DIV:
-    elmnt.onmousedown = dragMouseDown;
-  }
 
-  function dragMouseDown(e) {
-    e = e || window.event;
-    e.preventDefault();
-    // get the mouse cursor position at startup:
-    pos3 = e.clientX;
-    pos4 = e.clientY;
-    document.onmouseup = closeDragElement;
-    // call a function whenever the cursor moves:
-    document.onmousemove = elementDrag;
-  }
+numberThree.addEventListener('mouseover', function() {
+    
+    this.classList.add('unblur');
+});
 
-  function elementDrag(e) {
-    e = e || window.event;
-    e.preventDefault();
-    // calculate the new cursor position:
-    pos1 = pos3 - e.clientX;
-    pos2 = pos4 - e.clientY;
-    pos3 = e.clientX;
-    pos4 = e.clientY;
-    // set the element's new position:
-    elmnt.style.top = (elmnt.offsetTop - pos2) + "px";
-    elmnt.style.left = (elmnt.offsetLeft - pos1) + "px";
-  }
 
-  function closeDragElement() {
-    // stop moving when mouse button is released:
-    document.onmouseup = null;
-    document.onmousemove = null;
-  }
-}
+numberThree.addEventListener('mouseout', function() {
+   
+    this.classList.remove('unblur');
+});
+
+
+
+
+document.documentElement.style.cursor = 'url(/project1/imgs/blackcursor.png), auto';
+
+const slider = document.getElementById("slider");
+const text = document.getElementById("text");
+
+slider.addEventListener("input", function() {
+  // Calculate font size based on slider value (0 to 3em)
+  const fontSize = (this.value / 100) * 3;
+  // Set the font size of the text
+  text.style.fontSize = fontSize + "em";
+})
+
+
+
